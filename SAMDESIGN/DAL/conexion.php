6da18687bad992cf79b85ff9e3fbadc2c6cb35d7
Jorge Morua <jorge.morua@prestarte.cr>
2025-02-24 20:37:08 -0600

    PENDIENTE Arreglar SAM_DESIGN_SCHEMA con en base a los comments. Reestructuracion del archivo sql de SAM DESIGN creacion de usuario y agregarmos conexion

--- SAMDESIGN/DAL/conexion.php
+++ SAMDESIGN/DAL/conexion.php
@@ -2,20 +2,23 @@
 
 function conectar() {
 
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "sam_design";
+    $user = "FIDE_SAM_DESIGN";
+    $password = "SAMDESIGN";
+    $host = "localhost/XE";
+    // para conectar a la base de datos de oracle usamos el protocolo oci de php
+    $conexion = oci_connect($user, $password, $host);
 
-
-    $conexion = mysqli_connect($server, $user, $password, $database);
-
-    if(!$conexion){
-        echo("Ocurrio un error conectando a la base de datos: ". mysqli_connect_error());
-    }
+    if (!$conexion) {
+        $m = oci_error();
+        echo "<script>console.error('Database connection error: " . addslashes($m['message']) . "');</script>";
+        exit;
+     }
+     else {
+        // print "<script>console.log('Connected to Oracle!');</script>";//usamos sentencias de javascript para poder imprimir el mensaje  en consola
+     }
     return $conexion;
 }
 
 function desconectar($conexion){
-    mysqli_close($conexion);
+    oci_close($conexion);
 }
