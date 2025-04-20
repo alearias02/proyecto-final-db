87d90389619067fc7a3d330b2afa70d198ac6f24
HACKMYNUTTS <jorgemoruagonzalez@gmail.com>
2025-04-19 21:55:17 -0600

    added website icon

--- include/templates/header.php
+++ include/templates/header.php
@@ -22,6 +22,7 @@ if (isset($_SESSION['usuario'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SAM DESIGN</title>
+    <link rel="website icon" href="../img/logo_pequeño-removebg-preview.png" type="png">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
